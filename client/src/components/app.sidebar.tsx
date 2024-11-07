361466f521cbb0d566ca50f472f96d4442c34b64
bo <macabo10quymui@gmail.com>
2024-11-07 08:48:43 +0700

    Change container option to drop down list

--- client/src/components/app.sidebar.tsx
+++ client/src/components/app.sidebar.tsx
@@ -1,13 +1,22 @@
 'use client'
 
 import styled from '@emotion/styled';
-import { Drawer, Box, List, ListItem, ListItemButton, ListItemIcon, ListItemText, Divider, Typography } from '@mui/material';
+import { Drawer, Box, List, ListItem, ListItemButton, ListItemIcon, ListItemText, Divider, Typography, FormControl, Select, MenuItem, InputLabel, SelectChangeEvent } from '@mui/material';
 import { useTheme } from '@mui/material/styles';
 // import DashboardIcon from '@mui/icons-material/Dashboard';
 
 import Link from "next/link";
+import React from 'react';
 
 const AppSideBar = () => {
+    const [service, setService] = React.useState('');
+
+    const handleServiceChange = (event: SelectChangeEvent) => {
+        console.log(event.target.value);
+        setService(event.target.value);
+        
+    };
+    
     const theme = useTheme();
     return (
         <Drawer
@@ -43,25 +52,33 @@ const AppSideBar = () => {
                             {/* <DashboardIcon /> */}
                         </ListItemIcon>
                         <ListItemText primary="Dashboard"
-                            primaryTypographyProps={{ sx: { color: 'white', fontSize: '22px', fontWeight: 'normal' } }}
+                            primaryTypographyProps={{ sx: { color: 'white', fontSize: '22px', fontWeight: 'normal',  alignContent: 'center'} }}
                         />
                     </ListItemButton>
-                    <ListItemButton component={Link} href="/detail"
-                        sx={{
-                            '&:hover': {
-                                '& .MuiListItemText-primary': {
-                                    color: theme.palette.primary.main,
-                                },
-                            },
-                            padding: '8px 16px', // Adjust padding to make the button smaller
-                        }}
-                    >
-                        <ListItemIcon>
-                            {/* <DashboardIcon /> */}
-                        </ListItemIcon>
-                        <ListItemText primary="Detail"
-                            primaryTypographyProps={{ sx: { color: 'white', fontSize: '22px', fontWeight: 'normal' } }}
-                        />
+                    <ListItemButton>
+                        <FormControl 
+                            fullWidth
+                        >
+                            <InputLabel id="service-select-label"
+                                sx={{
+                                    color: 'white',
+                                    fontWeight: 'normal',
+                                    alignItems: 'center',
+                                }}
+                            >
+                                Services
+                            </InputLabel>
+                            <Select
+                                id="serivce-select"
+                                label="Services"
+                                value={service}
+                                sx={{color: 'white'}}
+                                onChange={handleServiceChange}
+                            >
+                                <MenuItem value={"gold-price-service"} sx={{color: 'white'}}>Gold price service</MenuItem>
+                                <MenuItem value={"exchange-rate-service"} sx={{color: 'white'}}>Exchange rate service</MenuItem>
+                            </Select>
+                        </FormControl>
                     </ListItemButton>
                 </List>
             </Box>

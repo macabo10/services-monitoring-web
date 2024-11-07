a0cf6903f753bb49294c588767ada05badf16b68
macabo10 <macabo10quymui@gmail.com>
2024-11-07 12:55:19 +0700

    Modify side bar

--- client/src/components/app.sidebar.tsx
+++ client/src/components/app.sidebar.tsx
@@ -1,30 +1,116 @@
 'use client'
 
 import styled from '@emotion/styled';
-import { Drawer, Box, List, ListItem, ListItemButton, ListItemIcon, ListItemText, Divider, Typography, FormControl, Select, MenuItem, InputLabel, SelectChangeEvent } from '@mui/material';
+import { Drawer, Box, List, ListItem, ListItemButton, ListItemIcon, ListItemText, Divider, Typography, Collapse } from '@mui/material';
 import { useTheme } from '@mui/material/styles';
-// import DashboardIcon from '@mui/icons-material/Dashboard';
+import { ExpandLess, ExpandMore } from '@mui/icons-material';
 
 import Link from "next/link";
-import React from 'react';
+import React, { ReactNode } from 'react';
+
+const CustomListItem = ({ href, title, onClick, children }: { href: string; title: string; onClick: () => void, children: ReactNode }) => {
+    const theme = useTheme();
+    return (
+        <ListItemButton
+            sx={{
+                '&:hover': {
+                    '& .MuiListItemText-primary': {
+                        color: theme.palette.primary.main,
+                    },
+                },
+            }}
+            onClick={onClick}
+            component={Link}
+            href={href}
+        >
+            <ListItemText
+                primary={title}
+                primaryTypographyProps={{
+                    sx: {
+                        color: 'white',
+                        fontSize: '22px',
+                        fontWeight: 'normal',
+                        alignContent: 'center',
+                        marginLeft: '2rem'
+                    },
+                }}
+            />
+            {children}
+        </ListItemButton>
+    );
+};
+
+const CollapseItemStyle = {
+    color: 'white',
+    fontSize: '22px',
+    fontWeight: 'normal',
+    alignContent: 'center',
+    pl: '4rem',
+}
+
+const SidebarList = () => {
+    const theme = useTheme();
+
+    const [open, setOpen] = React.useState(true);
+
+    const handleClick = () => {
+        setOpen(!open);
+    };
+
+    return (
+        <List>
+        <CustomListItem href='/dashboard' title='Dashboard'>
+        </CustomListItem>
+        <CustomListItem href='/' title='Services' onClick={handleClick}>
+        {open ? <ExpandLess sx={{ color: 'white' }} /> : <ExpandMore sx={{ color: 'white' }} />}
+
+        </CustomListItem> 
+        <Collapse in={open} timeout="auto" unmountOnExit>
+            <List component="div" disablePadding>
+            <ListItemButton
+                sx={{
+                    ...CollapseItemStyle,
+                    '&:hover': {
+                        '& .MuiListItemText-primary': {
+                            color: theme.palette.primary.main,
+                        },
+                    },
+                }}
+                href='/exchange-rate-service'
+            >
+                <ListItemText primary="Exchange rate service" />
+            </ListItemButton>
+            <ListItemButton
+                sx={{
+                    ...CollapseItemStyle,
+                    '&:hover': {
+                        '& .MuiListItemText-primary': {
+                            color: theme.palette.primary.main,
+                        },
+                    },
+                }}
+                href='/gold-price-service'
+            >
+                <ListItemText primary="Gold price service" />
+            </ListItemButton>
+            </List>
+        </Collapse>
+    </List>
+);
+
+    
+}
+
 
 const AppSideBar = () => {
-    const [service, setService] = React.useState('');
-
-    const handleServiceChange = (event: SelectChangeEvent) => {
-        console.log(event.target.value);
-        setService(event.target.value);
-        
-    };
-    
     const theme = useTheme();
+
     return (
         <Drawer
             variant="permanent"
             anchor="left"
             sx={{
                 '& .MuiDrawer-paper': {
-                    // width: drawerWidth,
                     boxSizing: 'border-box',
                     backgroundColor: theme.palette.background.default,
                 },
@@ -34,53 +120,10 @@ const AppSideBar = () => {
                 <Typography variant="h6" sx={{
                     padding: 2, fontSize: '33px', fontWeight: 'bold', color: 'white'
                 }}>
-                    MonitoringService
+                    Monitoring Service
                 </Typography>
                 <Divider />
-                <List>
-                    <ListItemButton component={Link}
-                        href="/dashboard"
-                        sx={{
-                            '&:hover': {
-                                '& .MuiListItemText-primary': {
-                                    color: theme.palette.primary.main,
-                                },
-                            },
-                        }}
-                    >
-                        <ListItemIcon>
-                            {/* <DashboardIcon /> */}
-                        </ListItemIcon>
-                        <ListItemText primary="Dashboard"
-                            primaryTypographyProps={{ sx: { color: 'white', fontSize: '22px', fontWeight: 'normal',  alignContent: 'center'} }}
-                        />
-                    </ListItemButton>
-                    <ListItemButton>
-                        <FormControl 
-                            fullWidth
-                        >
-                            <InputLabel id="service-select-label"
-                                sx={{
-                                    color: 'white',
-                                    fontWeight: 'normal',
-                                    alignItems: 'center',
-                                }}
-                            >
-                                Services
-                            </InputLabel>
-                            <Select
-                                id="serivce-select"
-                                label="Services"
-                                value={service}
-                                sx={{color: 'white'}}
-                                onChange={handleServiceChange}
-                            >
-                                <MenuItem value={"gold-price-service"} sx={{color: 'white'}}>Gold price service</MenuItem>
-                                <MenuItem value={"exchange-rate-service"} sx={{color: 'white'}}>Exchange rate service</MenuItem>
-                            </Select>
-                        </FormControl>
-                    </ListItemButton>
-                </List>
+                <SidebarList/>
             </Box>
         </Drawer >
     );

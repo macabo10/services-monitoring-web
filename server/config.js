df701798ee875a98c1ad508ce019ca1fe482af7f
tranngocbach <tranngocbach2003@gmail.com>
2024-11-17 16:36:05 +0700

    change config db

--- server/config.js
+++ server/config.js
@@ -1,20 +1,11 @@
-// const config = {
-//     db: {
-//         host: 'localhost',
-//         user: 'root',
-//         password: '1234',
-//         database: 'monitoring_service',
-//         port: 3307
-//     }
-// };
-
 const config = {
     db: {
         host: 'localhost',
         user: 'root',
-        password: 'Bach2003',
+        password: '1234',
         database: 'monitoring_service',
-        port: 3306
+        port: 3307
     }
 };
+
 module.exports = config;
